3fae194f3d78f1ed4cbb5f7d29b0432dd7c50c1d
unknownproperty <igor.buksheff@yandex.ru>
2023-06-08 22:20:45 +0300

    fix: build

--- packages/pkg.inputs.input/Input.tsx
+++ packages/pkg.inputs.input/Input.tsx
@@ -6,42 +6,40 @@ type InputProps = {
   error?: boolean;
 };
 
-export const Input = React.forwardRef<HTMLInputElement, TextFieldProps & InputProps>(
-  (props, ref) => {
-    const { sx, error, ...restProps } = props;
+export const Input = React.forwardRef<TextFieldProps, InputProps>((props, ref) => {
+  const { sx, error, ...restProps } = props;
 
-    return (
-      <TextField
-        sx={{
-          '& .MuiInputBase-root': {
-            borderRadius: '8px',
-            height: '56px',
-            fontWeight: 400,
-            fontSize: 16,
-            lineHeight: '20px',
-          },
-          '& .MuiInputBase-input::placeholder': {
-            opacity: 1,
-            color: `${error ? '#F42D2D' : '#999'}`,
-          },
-          '& .MuiFormHelperText-root': {
-            fontSize: 14,
-            marginLeft: 0,
-            fontWeight: 400,
-            color: '#F42D2D',
-            marginTop: '4px',
-            lineHeight: '16px',
-          },
-          '& .MuiOutlinedInput-notchedOutline': {
-            border: `1px solid ${error ? '#F42D2D' : '#E6E6E6'}`,
-          },
-          ...sx,
-        }}
-        ref={ref}
-        {...restProps}
-      />
-    );
-  },
-);
+  return (
+    <TextField
+      sx={{
+        '& .MuiInputBase-root': {
+          borderRadius: '8px',
+          height: '56px',
+          fontWeight: 400,
+          fontSize: 16,
+          lineHeight: '20px',
+        },
+        '& .MuiInputBase-input::placeholder': {
+          opacity: 1,
+          color: `${error ? '#F42D2D' : '#999'}`,
+        },
+        '& .MuiFormHelperText-root': {
+          fontSize: 14,
+          marginLeft: 0,
+          fontWeight: 400,
+          color: '#F42D2D',
+          marginTop: '4px',
+          lineHeight: '16px',
+        },
+        '& .MuiOutlinedInput-notchedOutline': {
+          border: `1px solid ${error ? '#F42D2D' : '#E6E6E6'}`,
+        },
+        ...sx,
+      }}
+      ref={ref}
+      {...restProps}
+    />
+  );
+});
 
 Input.displayName = 'Input';
