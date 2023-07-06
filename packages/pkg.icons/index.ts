84339e60a3358ea24e2238e0830066ecba703b73
KashVer <veronika.kashkadova55@gmail.com>
2023-07-06 19:49:40 +0300

    feat(11379943): add new icons from Figma

--- packages/pkg.icons/index.ts
+++ packages/pkg.icons/index.ts
@@ -1,23 +1,39 @@
 import { Account } from './src/Account';
 import { Activity } from './src/Activity';
 import { Add } from './src/Add';
-import { AddCategory } from './src/AddCategory';
-import { AddChannel } from './src/AddChannel';
 import { Announce } from './src/Announce';
-import { Arrow } from './src/Arrow';
+import { ArrowBottom } from './src/ArrowBottom';
+import { ArrowLeft } from './src/ArrowLeft';
+import { ArrowRight } from './src/ArrowRight';
+import { ArrowUp } from './src/ArrowUp';
 import { Bold } from './src/Bold';
+import { Brush } from './src/Brush';
 import { Burger } from './src/Burger';
 import { Calendar } from './src/Calendar';
 import { Camera } from './src/Camera';
+import { CategoryAdd } from './src/CategoryAdd';
+import { ChannelAdd } from './src/ChannelAdd';
 import { Chat } from './src/Chat';
 import { Check } from './src/Check';
+import { ChevronBottom } from './src/ChevronBottom';
+import { ChevronLeft } from './src/ChevronLeft';
+import { ChevronRight } from './src/ChevronRight';
+import { ChevronUp } from './src/ChevronUp';
+import { ChevronSmallBottom } from './src/ChevronSmallBottom';
+import { ChevronSmallLeft } from './src/ChevronSmallLeft';
+import { ChevronSmallRight } from './src/ChevronSmallRight';
+import { ChevronSmallTop } from './src/ChevronSmallTop';
 import { Cite } from './src/Cite';
 import { Clip } from './src/Clip';
 import { Clock } from './src/Clock';
 import { Close } from './src/Close';
 import { Code } from './src/Code';
+import { Collapse } from './src/Collapse';
+import { ColorPicker } from './src/ColorPicker';
 import { Copy } from './src/Copy';
+import { Divider } from './src/Divider';
 import { Download } from './src/Download';
+import { Edit } from './src/Edit';
 import { Emotions } from './src/Emotions';
 import { Endcall } from './src/Endcall';
 import { Exit } from './src/Exit';
@@ -25,6 +41,8 @@ import { External } from './src/External';
 import { Eyeoff } from './src/Eyeoff';
 import { Eyeon } from './src/Eyeon';
 import { File } from './src/File';
+import { FileSmall } from './src/FileSmall';
+import { Filter } from './src/Filter';
 import { Flag } from './src/Flag';
 import { Folder } from './src/Folder';
 import { Food } from './src/Food';
@@ -35,12 +53,18 @@ import { H3 } from './src/H3';
 import { Hand } from './src/Hand';
 import { Heart } from './src/Heart';
 import { Home } from './src/Home';
-import { Invite } from './src/Invite';
 import { Italic } from './src/Italic';
+import { Key } from './src/Key';
+import { Laptop } from './src/Laptop';
 import { Link } from './src/Link';
+import { Locked } from './src/Locked';
+import { LongAnswer } from './src/LongAnswer';
+import { Mail } from './src/Mail';
 import { Maximize } from './src/Maximize';
 import { Microphone } from './src/Microphone';
-import { Minus } from './src/Minus';
+import { Minimize } from './src/Minimize';
+import { Moon } from './src/Moon';
+import { MoreVert } from './src/MoreVert';
 import { Move } from './src/Move';
 import { Movie } from './src/Movie';
 import { Music } from './src/Music';
@@ -48,47 +72,81 @@ import { Nature } from './src/Nature';
 import { Notification } from './src/Notification';
 import { Objects } from './src/Objects';
 import { Ol } from './src/Ol';
-import { Palette } from './src/Palette';
+import { Pallet } from './src/Pallet';
+import { PeopleInvite } from './src/PeopleInvite';
 import { Photo } from './src/Photo';
 import { Picture } from './src/Picture';
 import { Places } from './src/Places';
 import { Play } from './src/Play';
+import { Presentation } from './src/Presentation';
+import { PushPin } from './src/PushPin';
+import { Reply } from './src/Reply';
 import { Screenshare } from './src/Screenshare';
 import { Search } from './src/Search';
+import { Section } from './src/Section';
 import { Send } from './src/Send';
 import { Settings } from './src/Settings';
 import { Shape } from './src/Shape';
+import { SimpleAnswer } from './src/SimpleAnswer';
+import { Sort } from './src/Sort';
 import { Stroke } from './src/Stroke';
+import { SoundOff } from './src/SoundOff';
+import { SoundOn } from './src/SoundOn';
+import { Speaker } from './src/Speaker';
+import { Sun } from './src/Sun';
 import { Task } from './src/Task';
+import { TaskFile } from './src/TaskFile';
+import { Telegram } from './src/Telegram';
 import { Text } from './src/Text';
 import { Trash } from './src/Trash';
 import { Ul } from './src/Ul';
 import { Underline } from './src/Underline';
+import { Unlocked } from './src/Unlocked';
+import { Upload } from './src/Upload';
 import { Updates } from './src/Updates';
 import { Users } from './src/Users';
+import { VariantSelect } from './src/VariantSelect';
+import { Video } from './src/Video';
+import { Window } from './src/Window';
 import { icons } from './config';
 
 export {
   Account,
   Activity,
   Add,
-  AddCategory,
-  AddChannel,
   Announce,
-  Arrow,
+  ArrowBottom,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
   Bold,
+  Brush,
   Burger,
   Calendar,
   Camera,
+  CategoryAdd,
+  ChannelAdd,
   Chat,
   Check,
+  ChevronRight,
+  ChevronBottom,
+  ChevronLeft,
+  ChevronUp,
+  ChevronSmallBottom,
+  ChevronSmallLeft,
+  ChevronSmallRight,
+  ChevronSmallTop,
   Cite,
   Clip,
   Clock,
   Close,
   Code,
+  Collapse,
+  ColorPicker,
   Copy,
+  Divider,
   Download,
+  Edit,
   Emotions,
   Endcall,
   Exit,
@@ -96,6 +154,8 @@ export {
   Eyeoff,
   Eyeon,
   File,
+  FileSmall,
+  Filter,
   Flag,
   Folder,
   Food,
@@ -106,12 +166,18 @@ export {
   Hand,
   Heart,
   Home,
-  Invite,
   Italic,
+  Key,
+  Laptop,
   Link,
+  Locked,
+  LongAnswer,
+  Mail,
   Maximize,
   Microphone,
-  Minus,
+  Minimize,
+  Moon,
+  MoreVert,
   Move,
   Movie,
   Music,
@@ -119,24 +185,42 @@ export {
   Notification,
   Objects,
   Ol,
-  Palette,
+  Pallet,
+  PeopleInvite,
   Photo,
   Picture,
   Places,
   Play,
+  Presentation,
+  PushPin,
+  Reply,
   Screenshare,
   Search,
+  Section,
   Send,
   Settings,
   Shape,
+  SimpleAnswer,
+  Sort,
+  SoundOff,
+  SoundOn,
+  Speaker,
+  Sun,
   Stroke,
   Task,
+  TaskFile,
+  Telegram,
   Text,
   Trash,
   Ul,
   Underline,
+  Unlocked,
+  Upload,
   Updates,
   Users,
+  VariantSelect,
+  Video,
+  Window,
   icons,
 };
 
