b31dc7f9d322f736292c27a3639e2c1e23d6efe1
mariik <m.portnykh@fxdev.sk>
2023-07-07 12:18:27 +0200

    feat: add components folder

---
+++ apps/xi.playground/app/components/button/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+export default function Button() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-1 p-24">
+      <button type="button" className="btn btn-s">
+        Click me
+      </button>
+      <button type="button" className="btn btn-m">
+        Click me
+      </button>
+      <button type="button" className="btn btn-l">
+        Click me
+      </button>
+      <button type="button" className="btn btn-s btn-primary">
+        Click me
+      </button>
+      <button type="button" className="btn btn-m btn-primary">
+        Click me
+      </button>
+      <button type="button" className="btn btn-l btn-primary">
+        Click me
+      </button>
+    </main>
+  );
+}
