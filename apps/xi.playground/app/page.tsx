21cd488edcb84bd05394f69b68831dfc6a066fa7
KashVer <veronika.kashkadova55@gmail.com>
2023-07-11 12:34:24 +0300

    feat(11379717): wip

--- apps/xi.playground/app/page.tsx
+++ apps/xi.playground/app/page.tsx
@@ -21,6 +21,34 @@ export default function Home() {
       <button type="button" className="btn btn-l btn-primary">
         Click me
       </button>
+      <a href="https://www.google.com/">I am a simple link</a>
+      <a href="https://www.google.com/" className="link">
+        I am a default link
+      </a>
+      <a href="https://www.google.com/" className="link link-s">
+        I am a small link
+      </a>
+      <a href="https://www.google.com/" className="link link-m">
+        I am a medium link
+      </a>
+      <a href="https://www.google.com/" className="link link-l">
+        I am a large link
+      </a>
+      <a href="https://www.google.com/" className="link link-l disabled m-5">
+        I am a disabled large link
+      </a>
+      <a href="https://www.google.com/" className="link link-l link-sky m-5">
+        I am a sky large link
+      </a>
+      <a href="https://www.google.com/" className="link link-l link-brand m-5">
+        I am a brand large link
+      </a>
+      <a href="https://www.google.com/" className="link link-l link-moscow m-5">
+        I am a moscow large link
+      </a>
+      <a href="https://www.google.com/" className="link link-l link-ekaterinburg m-5">
+        I am a ekaterinburg large link
+      </a>
     </main>
   );
 }
