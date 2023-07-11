2694d539134b5873ab84a27ff57cc25a3e2f3756
mariik <m.portnykh@fxdev.sk>
2023-07-12 00:13:03 +0200

    feat: add inputs colors

--- apps/xi.playground/app/components/input/page.tsx
+++ apps/xi.playground/app/components/input/page.tsx
@@ -1,9 +1,24 @@
 export default function Input() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-1 p-24">
+    <main className="flex min-h-screen flex-col items-center gap-8 p-24">
       <div className="flex flex-row gap-4">
         <input className="input input-m" />
         <input className="input input-s" />
+      </div>
+
+      <div className="flex flex-row gap-4">
+        <input className="input input-m input-error" />
+        <input className="input input-s input-error" />
+      </div>
+
+      <div className="flex flex-row gap-4">
+        <input className="input input-m input-warning" />
+        <input className="input input-s input-warning" />
+      </div>
+
+      <div className="flex flex-row gap-4">
+        <input className="input input-m" disabled />
+        <input className="input input-s" disabled />
       </div>
     </main>
   );
