96346573d8c166e331a36cca61e90719108c7a53
mariik <m.portnykh@fxdev.sk>
2023-07-11 18:11:32 +0200

    fix: change buttons position

--- apps/xi.playground/app/components/button/page.tsx
+++ apps/xi.playground/app/components/button/page.tsx
@@ -2,56 +2,78 @@
 
 export default function Button() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-1 p-24">
-      <button type="button" className="btn btn-s">
-        Click me
-      </button>
-      <button type="button" className="btn btn-m">
-        Click me
-      </button>
-      <button type="button" className="btn btn-l">
-        Click me
-      </button>
+    <main className="flex flex-col items-center gap-7 p-24">
+      <div className="flex flex-row gap-4">
+        <button type="button" className="btn btn-s">
+          Click me
+        </button>
+        <button type="button" className="btn btn-m">
+          Click me
+        </button>
+        <button type="button" className="btn btn-l">
+          Click me
+        </button>
+      </div>
 
-      <button type="button" className="btn btn-s btn-primary">
-        Click me
-      </button>
-      <button type="button" className="btn btn-m btn-primary">
-        Click me
-      </button>
-      <button type="button" className="btn btn-l btn-primary">
-        Click me
-      </button>
+      <div className="flex flex-row gap-4">
+        <button type="button" className="btn btn-s" disabled>
+          Click me
+        </button>
+        <button type="button" className="btn btn-m" disabled>
+          Click me
+        </button>
+        <button type="button" className="btn btn-l" disabled>
+          Click me
+        </button>
+      </div>
 
-      <button type="button" className="btn btn-s btn-error">
-        Click me
-      </button>
-      <button type="button" className="btn btn-m btn-error">
-        Click me
-      </button>
-      <button type="button" className="btn btn-l btn-error">
-        Click me
-      </button>
+      <div className="flex flex-row gap-4">
+        <button type="button" className="btn btn-s btn-primary">
+          Click me
+        </button>
+        <button type="button" className="btn btn-m btn-primary">
+          Click me
+        </button>
+        <button type="button" className="btn btn-l btn-primary">
+          Click me
+        </button>
+      </div>
 
-      <button type="button" className="btn btn-s btn-success">
-        Click me
-      </button>
-      <button type="button" className="btn btn-m btn-success">
-        Click me
-      </button>
-      <button type="button" className="btn btn-l btn-success">
-        Click me
-      </button>
+      <div className="flex flex-row gap-4">
+        <button type="button" className="btn btn-s btn-error">
+          Click me
+        </button>
+        <button type="button" className="btn btn-m btn-error">
+          Click me
+        </button>
+        <button type="button" className="btn btn-l btn-error">
+          Click me
+        </button>
+      </div>
 
-      <button type="button" className="btn btn-s btn-outlined">
-        Click me
-      </button>
-      <button type="button" className="btn btn-m btn-outlined">
-        Click me
-      </button>
-      <button type="button" className="btn btn-l btn-outlined">
-        Click me
-      </button>
+      <div className="flex flex-row gap-4">
+        <button type="button" className="btn btn-s btn-success">
+          Click me
+        </button>
+        <button type="button" className="btn btn-m btn-success">
+          Click me
+        </button>
+        <button type="button" className="btn btn-l btn-success">
+          Click me
+        </button>
+      </div>
+
+      <div className="flex flex-row gap-4">
+        <button type="button" className="btn btn-s btn-outlined">
+          Click me
+        </button>
+        <button type="button" className="btn btn-m btn-outlined">
+          Click me
+        </button>
+        <button type="button" className="btn btn-l btn-outlined">
+          Click me
+        </button>
+      </div>
     </main>
   );
 }
