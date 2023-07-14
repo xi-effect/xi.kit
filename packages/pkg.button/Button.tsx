7514877a1e7c9b217f9943a913ed30e949ceb315
mariik <m.portnykh@fxdev.sk>
2023-07-14 13:24:52 +0200

    feat: add btn color

--- packages/pkg.button/Button.tsx
+++ packages/pkg.button/Button.tsx
@@ -1,8 +1,18 @@
 import { ComponentProps } from 'react';
 
-export type ButtonProps = {} & ComponentProps<'button'>;
+export type ButtonColors = 'primary' | 'success' | 'error';
+export type ButtonProps = {
+  color?: ButtonColors;
+} & ComponentProps<'button'>;
 
-export const Button = ({ type = 'button' }: ButtonProps) => {
-  console.log('Button');
-  return <button type={type}>Button</button>;
+export const Button = ({ type = 'button', color = 'primary' }: ButtonProps) => {
+  const colorClass =
+    (color === 'primary' && 'btn-primary') ||
+    (color === 'success' && 'btn-success') ||
+    (color === 'error' && 'btn-error');
+  return (
+    <button type={type} className={`btn ${colorClass}`}>
+      Button
+    </button>
+  );
 };
