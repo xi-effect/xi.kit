b613aea557137b79d2e1315643aa126bb791c0d3
mariik <m.portnykh@fxdev.sk>
2023-07-14 13:31:22 +0200

    feat: add btn sizes

--- packages/pkg.button/Button.tsx
+++ packages/pkg.button/Button.tsx
@@ -1,17 +1,23 @@
 import { ComponentProps } from 'react';
 
 export type ButtonColors = 'primary' | 'success' | 'error';
+export type ButtonSizes = 's' | 'm' | 'l';
 export type ButtonProps = {
   color?: ButtonColors;
+  size?: ButtonSizes;
 } & ComponentProps<'button'>;
 
-export const Button = ({ type = 'button', color = 'primary' }: ButtonProps) => {
+export const Button = ({ type = 'button', color = 'primary', size = 'm' }: ButtonProps) => {
   const colorClass =
     (color === 'primary' && 'btn-primary') ||
     (color === 'success' && 'btn-success') ||
     (color === 'error' && 'btn-error');
+
+  const sizeClass =
+    (size === 's' && 'btn-s') || (size === 'm' && 'btn-m') || (size === 'l' && 'btn-l');
+
   return (
-    <button type={type} className={`btn ${colorClass}`}>
+    <button type={type} className={`btn ${colorClass} ${sizeClass}`}>
       Button
     </button>
   );
