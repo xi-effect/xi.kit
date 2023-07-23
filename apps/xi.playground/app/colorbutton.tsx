e784512b1e8d07c1147813f493fb1ad58ce8b87c
mariik <m.portnykh@fxdev.sk>
2023-07-23 22:57:18 +0200

    fix: mode btn

--- apps/xi.playground/app/colorbutton.tsx
+++ apps/xi.playground/app/colorbutton.tsx
@@ -1,29 +1,21 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { Switch, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Button } from '@xipkg/button';
 
 const ColorButton = () => {
   const { theme, setTheme } = useTheme();
-
-  const [darkMode, setDarkMode] = useState<boolean>(theme === 'dark');
 
   const changeTheme = () => {
     if (theme === 'light') return setTheme('dark');
     return setTheme('light');
   };
 
-  const onChangeMode = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.checked;
-    changeTheme();
-    setDarkMode(value);
-  };
-
   return (
-    <div className="flex flex-row space-10-px items-center ml-auto">
-      <Typography variant="m">Mode:</Typography>
-      <Switch checked={darkMode} onChange={onChangeMode} defaultChecked={theme === 'dark'} />
+    <div className="flex flex-row space-10-px items-center ml-auto p-2">
+      <Button onClick={changeTheme} variant="outlined">
+        Mode
+      </Button>
     </div>
   );
 };
