ff1a755f4e2f74556a36edf14aa882d498c91709
unknownproperty <igor.buksheff@yandex.ru>
2023-07-26 16:03:08 +0300

    feat: some input changes

--- apps/xi.playground/app/components/input/page.tsx
+++ apps/xi.playground/app/components/input/page.tsx
@@ -1,16 +1,41 @@
 'use client';
 
 import { Stack } from '@mui/material';
+import { Eyeon } from '@xipkg/icons';
 import { Input } from '@xipkg/input';
 
 export default function InputPage() {
   return (
     <main>
-      <Stack sx={{ p: 4, width: '250px' }}>
+      <Stack sx={{ p: 2, width: '250px' }}>
         <Input />
       </Stack>
-      <Stack sx={{ p: 4, width: '250px' }}>
-        <Input placeholder="svasfvav" />
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input startAdornment={<Eyeon sx={{ fontSize: 24 }} />} placeholder="Текстовое поле" />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input startAdornment={<Eyeon />} placeholder="Текстовое поле" error />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input startAdornment={<Eyeon />} placeholder="Текстовое поле" warning />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input startAdornment={<Eyeon />} placeholder="Текстовое поле" disabled />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input size="small" />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input placeholder="Текстовое поле" size="small" />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input placeholder="Текстовое поле" size="small" error />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input placeholder="Текстовое поле" size="small" warning />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input placeholder="Текстовое поле" size="small" disabled />
       </Stack>
     </main>
   );
--- packages/pkg.input/Input.tsx
+++ packages/pkg.input/Input.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import InputBase, { InputBaseProps } from '@mui/material/InputBase';
 
-export type InputProps = InputBaseProps;
+export type InputProps = InputBaseProps & {
+  warning?: boolean;
+};
 
-export const Input = (props: InputProps) => {
-
+export const Input = ({ ...props }: InputProps) => {
   return <InputBase tabIndex={1} {...props} />;
 };
