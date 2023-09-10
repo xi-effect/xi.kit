dfa42e6d1a0e512aa1474c94cbecefc2ad5d3477
unknownproperty <igor.buksheff@yandex.ru>
2023-09-10 14:31:06 +0300

    feat: add Inter

--- apps/xi.playground/app/layout.tsx
+++ apps/xi.playground/app/layout.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import '@xipkg/tailwind/index.css'
-
+import { Inter } from 'next/font/google';
 import { Providers } from './providers';
 import { LayoutPages } from 'kit/LayoutPages';
 
+const inter = Inter({
+  weight: ['400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
 export default function RootLayout({ children }: any) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable}`}>
       <body style={{ display: 'flex', width: '100vw', minHeight: '100%' }}>
         <Providers>
           <LayoutPages>{children}</LayoutPages>
