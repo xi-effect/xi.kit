7558787ad2f028c803acb585ae8ca723e6e83e80
unknownproperty <igor.buksheff@yandex.ru>
2023-09-27 20:05:29 +0300

    feat: adaptive icons for dark theme

--- packages/pkg.icons/Svg.tsx
+++ packages/pkg.icons/Svg.tsx
@@ -5,25 +5,22 @@ import { cn } from '@xipkg/utils';
 
 
 export const svgVariants = cva(
-  'inline-flex items-center w-fit justify-center ring-offset-gray-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  '',
   {
     variants: {
       theme: {
-        default: 'text-gray-0 dark:text-gray-100 bg-brand-80 hover:bg-brand-100 active:bg-brand-100 focus:bg-brand-100',
-        secondary: 'bg-gray-0 text-gray-100 border-gray-30 border-2 hover:bg-gray-5 active:bg-gray-5 focus:bg-gray-5',
-        ghost: 'text-gray-100 bg-gray-0 border-0 hover:bg-gray-5 active:bg-gray-5 focus:bg-gray-5',
-        error: 'bg-red-80 text-gray-0 hover:bg-red-100 active:bg-red-100 focus:bg-red-100',
-        success: 'bg-green-80 text-gray-0 hover:bg-green-100 active:bg-green-100 focus:bg-green-100'
+        default: 'fill-gray-100',
       },
       size: {
-        l: 'h-14 px-8 rounded-xl text-[20px]',
-        m: 'h-12 px-6 rounded-lg text-[16px]',
-        s: 'h-8 px-4 rounded-md text-[14px]',
+        default: 'h-[24px] w-[24px]',
+        l: 'h-[32px] w-[32px]',
+        m: 'h-[20px] w-[20px]',
+        s: 'h-[16px] w-[16px]',
       },
     },
     defaultVariants: {
       theme: 'default',
-      size: 'l',
+      size: 'default',
     },
   },
 );
