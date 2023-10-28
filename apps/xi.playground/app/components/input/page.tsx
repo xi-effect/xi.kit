875b3ceaf5cb550db54b97442ff1bad696dcc184
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-10-28 23:56:52 +0300

    feat(19233638): add new input variants to the page

--- apps/xi.playground/app/components/input/page.tsx
+++ apps/xi.playground/app/components/input/page.tsx
@@ -2,12 +2,26 @@
 
 import { Stack } from '@mui/material';
 import { Input } from '@xipkg/input';
+import beforeIcon from '../../../public/beforeIcon.svg';
+import afterIcon from '../../../public/afterIcon.svg';
 
 export default function InputPage() {
   return (
     <>
       <Stack sx={{ p: 2, width: '250px' }}>
-        <Input />
+        <Input before={<img src={beforeIcon.src} />} />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input variant="s" before={<img src={beforeIcon.src} width={16} height={16} />} />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input after={<img src={afterIcon.src} />} />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input variant="s" after={<img src={afterIcon.src} width={16} height={16} />} />
+      </Stack>
+      <Stack sx={{ p: 2, width: '250px' }}>
+        <Input after="минут" />
       </Stack>
       <Stack sx={{ p: 2, width: '250px' }}>
         <Input placeholder="Текстовое поле" />
