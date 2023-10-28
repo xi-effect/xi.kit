8e03bd1ff6330b4fee1aa48d2765a8ec949b1f30
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-10-29 00:04:07 +0300

    fix(19233638): fix disables state styles

--- packages/pkg.input/Input.tsx
+++ packages/pkg.input/Input.tsx
@@ -4,12 +4,16 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@xipkg/utils';
 
 export const inputVariants = cva(
-  'flex items-center w-full rounded-md border-2 border-gray-30 bg-gray-0 text-gray-80 hover:border-gray-50 active:border-gray-30 px-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-30 disabled:cursor-not-allowed disabled:bg-gray-10',
+  'flex items-center w-full rounded-md border-2 border-gray-30 bg-gray-0 text-gray-80 hover:border-gray-50 active:border-gray-30 px-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-30',
   {
     variants: {
       variant: {
         m: 'h-12 px-3 rounded-lg text-[16px]',
         s: 'h-8 px-2 rounded-md text-[14px]',
+      },
+      disabled: {
+        true: 'cursor-not-allowed bg-gray-10 border-transparent hover:border-transparent active:border-transparent',
+        false: '',
       },
       focus: {
         true: 'border-gray-80 hover:border-gray-80 outline-none',
@@ -34,7 +38,7 @@ export const inputVariants = cva(
       afterString: {
         true: 'gap-x-2',
         false: '',
-      }
+      },
     },
     compoundVariants: [
       {
@@ -50,25 +54,26 @@ export const inputVariants = cva(
     ],
     defaultVariants: {
       variant: 'm',
+      disabled:false,
       error: false,
       warning: false,
       before: false,
       focus: false,
       after: false,
-      afterString: false
+      afterString: false,
     },
   },
 );
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
-    Omit<VariantProps<typeof inputVariants>, 'before' | 'after' | 'afterString'> {
+    Omit<VariantProps<typeof inputVariants>, 'before' | 'after' | 'afterString' | 'disabled'> {
   before?: JSX.Element | string;
   after?: JSX.Element | string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, variant, error, warning, type, before, after, onFocus, onBlur, ...props }, ref) => {
+  ({ className, variant, error, warning, type, before, after, onFocus, onBlur, disabled,...props }, ref) => {
     const [focus, setFocus] = React.useState<boolean>(false);
 
     const handleFocus = (e: React.FocusEvent<HTMLInputElement, Element>) => {
@@ -86,26 +91,28 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         className={cn(
           inputVariants({
             variant,
+            disabled,
             error,
             warning,
             className,
             before: Boolean(before),
             after: Boolean(after),
             focus,
-            afterString: typeof after === 'string'
+            afterString: typeof after === 'string',
           }),
         )}
       >
         <div>{before}</div>
         <input
+          disabled={disabled}
           onBlur={handleBlur}
           onFocus={handleFocus}
-          className="border-none outline-none h-full py-2 w-full"
+          className="border-none outline-none h-full py-2 w-full disabled:cursor-not-allowed"
           type={type}
           ref={ref}
           {...props}
         />
-        <div className='text-gray-60'>{after}</div>
+        <div className="text-gray-60">{after}</div>
       </div>
     );
   },
