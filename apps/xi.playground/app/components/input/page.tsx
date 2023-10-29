85465e0d5c6365f830ca2cc44d89127e8bbec622
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-10-29 19:06:55 +0300

    feat(19233638): remove stack component

--- apps/xi.playground/app/components/input/page.tsx
+++ apps/xi.playground/app/components/input/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Stack } from '@mui/material';
 import { Input } from '@xipkg/input';
 import beforeIcon from '../../../public/beforeIcon.svg';
 import afterIcon from '../../../public/afterIcon.svg';
@@ -8,39 +7,39 @@ import afterIcon from '../../../public/afterIcon.svg';
 export default function InputPage() {
   return (
     <>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      <div className="w-[250px] p-4">
         <Input before={<img src={beforeIcon.src} />} />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input variant="s" before={<img src={beforeIcon.src} width={16} height={16} />} />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input after={<img src={afterIcon.src} />} />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input variant="s" after={<img src={afterIcon.src} width={16} height={16} />} />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input after="минут" />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input placeholder="Текстовое поле" />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
-        <Input placeholder="Текстовое поле" variant="s"/>
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
+        <Input placeholder="Текстовое поле" variant="s" />
+      </div>
+      <div className="w-[250px] p-4">
         <Input placeholder="Текстовое поле" warning />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input placeholder="Текстовое поле" error />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input placeholder="Текстовое поле" disabled />
-      </Stack>
-      <Stack sx={{ p: 2, width: '250px' }}>
+      </div>
+      <div className="w-[250px] p-4">
         <Input />
-      </Stack>
+      </div>
     </>
   );
 }
