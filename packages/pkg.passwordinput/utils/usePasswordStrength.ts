721c366cd38bb38dd92d8866a5f21167e4237a0f
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-11-24 17:52:35 +0300

    feat(24744813): change password strength hook

--- packages/pkg.passwordinput/utils/usePasswordStrength.ts
+++ packages/pkg.passwordinput/utils/usePasswordStrength.ts
@@ -1,97 +1,84 @@
-import { useEffect, useMemo, useState } from 'react';
-import { commonPasswords } from './CommonPasswords';
-
-const RexExpValidation = (password: string, validation: string) =>
-  new RegExp(validation).test(password);
+import { useMemo, useState } from 'react';
+import { useDebounce } from './useDebounce';
 
 const MIN_PASSWORD_LENGTH = 6;
-const VALIDATIONS = (password: string) => [
-  {
-    passed: RexExpValidation(password, '[^A-Za-z0-9]'),
-    helper: 'Пароль должен содержать хотя бы один специальный символ',
-  },
-  {
-    passed: RexExpValidation(password, '[0-9]'),
-    helper: 'Пароль должен содержать хотя бы одну цифру',
-  },
-  {
-    passed: RexExpValidation(password, '[A-Z]'),
-    helper: 'Пароль должен содержать хотя бы одну прописную букву',
-  },
-  {
-    passed: RexExpValidation(password, '[a-z]'),
-    helper: 'Пароль должен содержать хотя бы одну строчную букву',
-  },
-  {
-    passed: password.length >= MIN_PASSWORD_LENGTH,
-    helper: 'Слишком короткий пароль, добавьте символов',
+const calculateStrength = (value: string): { message: string; strength: number } => {
+  const weakPasswordMessage = { message: 'Такой пароль легко взломать', strength: 25 };
+  const mediumPasswordMessage = {
+    message: 'Слишком короткий пароль, добавьте символов',
+    strength: 50,
+  };
+  const strongPasswordMessage = { message: 'Надежный пароль', strength: 100 };
+  switch (true) {
+    case value.length === 0:
+      return { message: 'Минимум 6 символов', strength: 0 };
+    case value.length < MIN_PASSWORD_LENGTH:
+      return {
+        message: `Слишком короткий пароль, добавьте ${MIN_PASSWORD_LENGTH - value.length} символов`,
+        strength: Math.ceil((value.length * 20) / MIN_PASSWORD_LENGTH),
+      };
+    case /^[0-9]*$/.test(value): {
+      if (value.length >= 13) return strongPasswordMessage;
+      else if (value.length === 11 || value.length === 12) {
+        const strength = value.length === 11 ? 50 : 60;
+        return { message: 'Слишком короткий пароль, добавьте символов', strength };
+      } else {
+        return weakPasswordMessage;
+      }
+    }
+    case /^[a-z]*$/.test(value): {
+      if (value.length >= 9) return strongPasswordMessage;
+      else if (value.length === 8) {
+        return mediumPasswordMessage;
+      } else {
+        return weakPasswordMessage;
+      }
+    }
+    case /^[A-Za-z]*$/.test(value): {
+      if (value.length >= 8) return strongPasswordMessage;
+      else if (value.length === 7) {
+        return mediumPasswordMessage;
+      } else {
+        return weakPasswordMessage;
+      }
+    }
+    case /^[0-9A-Za-z]*$/.test(value): {
+      if (value.length >= 7) return strongPasswordMessage;
+      else if (value.length === 6) {
+        return mediumPasswordMessage;
+      } else {
+        return weakPasswordMessage;
+      }
+    }
+    case /^[0-9A-Za-z!@#$&()`.+,\/"-;<>|~\\]*$/.test(value): {
+      if (value.length >= 7) return strongPasswordMessage;
+      else if (value.length === 6) {
+        return mediumPasswordMessage;
+      } else {
+        return weakPasswordMessage;
+      }
+    }
+    default:
+      return { message: 'Минимум 6 символов', strength: 0 };
   }
-];
+};
 
 export const usePasswordStrength = () => {
-  const [strengthValue, setStrengthValue] = useState<number>(0);
-  const [error, setError] = useState<string>('Минимум 6 символов');
-  const [weakPassword, setWeakPassword] = useState<boolean>(false);
   const [password, setPassword] = useState<string>('');
-
+  const debouncedPassword = useDebounce(password, 500);
+  const { message, strength } = useMemo(
+    () => calculateStrength(debouncedPassword),
+    [debouncedPassword],
+  );
+  const isWeak = useMemo(() => strength === 25, [strength]);
   const color = useMemo(() => {
-    if (strengthValue === 0) return { bar: 'gray-30', text: 'gray-60' };
-    if (strengthValue <= 25) {
-      return { bar: 'red-100', text: `${weakPassword ? 'red-100' : 'gray-60'}` };
+    if (strength === 0) return { bar: 'gray-30', text: 'gray-60' };
+    if (strength <= 25) {
+      return { bar: 'red-100', text: 'red-100' };
     }
-    if (strengthValue > 25 && strengthValue < 80) return { bar: 'orange-100', text: 'orange-100' };
+    if (strength > 25 && strength < 80) return { bar: 'orange-100', text: 'orange-100' };
     return { bar: 'green-100', text: 'green-100' };
-  }, [strengthValue]);
+  }, [strength]);
 
-  const checkStrength = () => {
-    setWeakPassword(false);
-    if (password) {
-      const validations = VALIDATIONS(password);
-
-      const passedValidation = validations.filter((validation) => {
-        if (!validation.passed) setError(validation.helper);
-        return validation.passed;
-      });
-
-      const strengthCoefficient = passedValidation.length / validations.length;
-      const strength = Math.round(strengthCoefficient * 100);
-      if (passedValidation.length === validations.length) {
-        setError('Надежный пароль');
-      }
-      setStrengthValue(strength);
-    } else {
-      setStrengthValue(0);
-      setError('Минимум 6 символов');
-    }
-  };
-
-  const updatePassword = (value: string) => setPassword(value);
-
-  const checkWeakPass = () => {
-    if (strengthValue >= 80 || weakPassword) {
-      const isWeak = commonPasswords.some((commonPass) => {
-        const lowerCommonPass = commonPass.toLowerCase();
-        const lowerPassword = password?.toLowerCase() || '';
-        return lowerPassword.includes(lowerCommonPass);
-      });
-
-      if (isWeak) {
-        setWeakPassword(true);
-        setError('Такой пароль легко взломать');
-        setStrengthValue(25);
-      } else {
-        setWeakPassword(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    checkStrength();
-  }, [password]);
-
-  useEffect(() => {
-    checkWeakPass();
-  }, [strengthValue, password]);
-
-  return { password, updatePassword, strengthValue, error, color, weakPassword };
+  return { password, setPassword, strength, message, color, isWeak };
 };
