eb49b83921d1730b814d02b690136442a1d5fa52
Andrey <andrey9191919@yandex.ru>
2023-12-09 10:03:53 +0300

    feat(26311698): restyled

--- apps/xi.playground/app/components/checkbox/page.tsx
+++ apps/xi.playground/app/components/checkbox/page.tsx
@@ -4,14 +4,30 @@ import { Checkbox } from '@xipkg/checkbox';
 
 export default function CheckboxDemo() {
   return (
-    <div className="flex items-center space-x-2">
-      <Checkbox id="terms" />
-      <label
-        htmlFor="terms"
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-      >
-        Accept terms and conditions
-      </label>
+    <div className="flex flex-col items-center gap-4">
+      <Checkbox size="l">Peepepe</Checkbox>
+      <Checkbox size="m">Peepepe</Checkbox>
+      <Checkbox size="s" checked>
+        Peepepe
+      </Checkbox>
+      <Checkbox size="l" disabled>
+        Peepepe
+      </Checkbox>
+      <Checkbox size="l" checked disabled>
+        Peepepe
+      </Checkbox>
+      <Checkbox state="varning" size="l">
+        Peepepe
+      </Checkbox>
+      <Checkbox state="varning" checked size="l">
+        Peepepe
+      </Checkbox>
+      <Checkbox state="error" size="l">
+        Peepepe
+      </Checkbox>
+      <Checkbox state="error" checked size="l">
+        Peepepe
+      </Checkbox>
     </div>
   );
 }
