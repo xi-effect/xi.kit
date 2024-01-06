57180a4a206feb6960eec6c3f75c378003c4f049
unknownproperty <igor.buksheff@yandex.ru>
2024-01-07 02:11:39 +0300

    feat: update Modal

--- packages/pkg.modal/Modal.tsx
+++ packages/pkg.modal/Modal.tsx
@@ -72,7 +72,7 @@ export const dialogCloseButtonVariants = cva('fixed', {
   variants: {
     view: {
       default:
-        'top-8 right-6 sm:top-6 sm:right-[-56px] flex flex-row w-10 h-10 items-start pt-2 px-2 rounded-[20px] bg-gray-5 sm:bg-gray-80',
+        'top-8 right-6 sm:top-6 sm:right-[-56px] flex flex-row w-10 h-10 items-start pt-2 px-2 rounded-[20px] bg-gray-5',
       full: 'left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 bg-gray-0 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
     },
   },
