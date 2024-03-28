dd67c57ba1c641dc6f931821eabd7b83d354f81d
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-03-28 21:18:49 +0100

    fix(29219183): fix debouncers

--- packages/pkg.utils/index.ts
+++ packages/pkg.utils/index.ts
@@ -1,6 +1,6 @@
 export { cn } from './cn';
+export * from './useDebounce';
+export * from './useDebouncedFunction';
 export * from './useSessionStorage';
 export * from './plural';
 export * from './format';
-export * from './useDebounce';
-export * from './useDebouncedFunction';
