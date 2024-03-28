82e33b7eecf590dda7eca9f81bf413bbd5413aab
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-03-28 18:28:58 +0100

    feat(29219183): add bouncers

--- packages/pkg.utils/index.ts
+++ packages/pkg.utils/index.ts
@@ -2,3 +2,5 @@ export { cn } from './cn';
 export * from './useSessionStorage';
 export * from './plural';
 export * from './format';
+export * from './useDebounce';
+export * from './useDebouncedFunction';
