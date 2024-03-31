0e4a6d7807d2cbc4e6cdd2317b08e0de801e9af8
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-03-31 13:33:51 +0200

    feat(30954117): add pkg.router.url from xi.app

---
+++ packages/pkg.router.url/deleteQuery.ts
@@ -0,0 +1,14 @@
+import { ReadonlyURLSearchParams } from 'next/navigation';
+
+type DeleteQueryT = {
+  [key: string]: string;
+};
+
+export const deleteQuery = (
+  searchParams: ReadonlyURLSearchParams | DeleteQueryT | string,
+  name: string,
+) => {
+  const params = new URLSearchParams(searchParams.toString());
+  params.delete(name);
+  return params.toString();
+};
