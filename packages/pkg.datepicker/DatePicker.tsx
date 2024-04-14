d334a4179f26d6f1f83c83a19a2cd9d69f0924eb
unknownproperty <igor.buksheff@yandex.ru>
2024-04-14 19:45:31 +0300

    feat: add props for Popover in DatePicker

--- packages/pkg.datepicker/DatePicker.tsx
+++ packages/pkg.datepicker/DatePicker.tsx
@@ -11,20 +11,29 @@ type DatePickerPropsT = {
   children: React.ReactNode;
   classNamePopoverTrigger?: string;
   classNamePopoverContent?: string;
+  popoverProps: React.ComponentProps<typeof Popover>;
+  popoverTriggerProps: React.ComponentProps<typeof PopoverTrigger>;
+  popoverContentProps: React.ComponentProps<typeof PopoverContent>;
 } & React.ComponentProps<typeof Calendar>;
 
 const DatePicker = ({
   children,
   classNamePopoverTrigger,
   classNamePopoverContent,
+  popoverProps,
+  popoverTriggerProps,
+  popoverContentProps,
   ...calendarProps
 }: DatePickerPropsT) => {
   return (
-    <Popover>
-      <PopoverTrigger className={classNamePopoverTrigger} asChild>
+    <Popover {...popoverProps}>
+      <PopoverTrigger className={classNamePopoverTrigger} asChild {...popoverTriggerProps}>
         {children}
       </PopoverTrigger>
-      <PopoverContent className={cn('w-auto p-0', classNamePopoverContent)}>
+      <PopoverContent
+        className={cn('w-auto p-0', classNamePopoverContent)}
+        {...popoverContentProps}
+      >
         <Calendar {...calendarProps} locale={ru} />
       </PopoverContent>
     </Popover>
