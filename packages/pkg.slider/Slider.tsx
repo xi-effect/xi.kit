c30e1ead6a9f1cccc05ee0a4b0ae0a8377e20984
d9m0n4 <ched08@yandex.ru>
2024-06-30 20:24:33 +0300

    fix(34683476): fix slider thumb cursor/playground bg

--- packages/pkg.slider/Slider.tsx
+++ packages/pkg.slider/Slider.tsx
@@ -17,7 +17,10 @@ export const Slider = React.forwardRef<
     <SliderPrimitive.Track className="bg-gray-30 relative h-1 w-full grow overflow-hidden rounded-lg">
       <SliderPrimitive.Range className="bg-brand-80 absolute h-full" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="bg-brand-80 ring-offset-background focus-visible:ring-ring block h-4 w-4 rounded-full transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="bg-brand-80 ring-offset-background block h-4 w-4 cursor-pointer rounded-full transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
+{
+  /* <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" /> */
+}
