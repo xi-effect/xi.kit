19f08b7cd9aa51c47beb7db30c6857be703aa9c2
Maxim <maksim.efremof@gmail.com>
2024-10-26 22:17:51 +0300

    feat(39526328): icons for test create

---
+++ packages/pkg.icons/icons/DoubleBottomArrows.tsx
@@ -0,0 +1,14 @@
+import { Svg, IconProps } from '../Svg';
+
+export const DoubleBottomArrows = ({ ...props }: IconProps) => (
+  <Svg {...props}>
+    <path
+      d="M17 12.0504L12.0503 17.0002L7.10051 12.0504M17 7.10071L12.0503 12.0505L7.1005 7.10071"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      fill="none"
+    />
+  </Svg>
+);
---
+++ packages/pkg.icons/icons/DoubleUpArrows.tsx
@@ -0,0 +1,14 @@
+import { Svg, IconProps } from '../Svg';
+
+export const DoubleUpArrows = ({ ...props }: IconProps) => (
+  <Svg {...props}>
+    <path
+      d="M7 11.9496L11.9497 6.99984L16.8995 11.9496M7 16.8993L11.9497 11.9495L16.8995 16.8993"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      fill="none"
+    />
+  </Svg>
+);
