c8fe18dd548dc19864050fb18eb137da184a4c88
Temich21 <rahmat97@mail.ru>
2024-11-07 22:16:43 +0100

    feat: add new functionality to textarea component

--- apps/xi.playground/app/components/textarea/page.tsx
+++ apps/xi.playground/app/components/textarea/page.tsx
@@ -1,66 +1,23 @@
 'use client';
 
 import { Textarea } from '@xipkg/textarea';
-import { Eyeoff, Eyeon } from '@xipkg/icons';
-import { Label } from '@xipkg/label';
-import { Link, CrossCircle } from '@xipkg/icons';
 import React from 'react';
 
 export default function InputPage() {
-  const [isPasswordShow, setIsPasswordShow] = React.useState(false);
-
-  const changePasswordShow = () => {
-    console.log('isPasswordShow');
-    setIsPasswordShow((prev) => !prev);
-  };
-
   return (
-    <>
+    <div className='flex flex-col gap-4'>
       <Textarea />
-      {/* <div className="w-[250px] p-4">
-        <Textarea before={<Link />} />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea
-          afterClassName="cursor-pointer"
-          type={isPasswordShow ? 'text' : 'password'}
-          after={isPasswordShow ? <Eyeoff /> : <Eyeon />}
-          afterProps={{
-            onClick: changePasswordShow,
-          }}
-        />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea variant="s" before={<Link size="s" />} />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea after={<CrossCircle />} />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea variant="s" after={<CrossCircle size="s" />} />
-      </div>
-      <div className="w-[250px] p-4">
-        <Label>Username</Label>
-        <Textarea after="минут" />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea placeholder="Текстовое поле" />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea placeholder="Текстовое поле" variant="s" />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea placeholder="Текстовое поле" warning />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea placeholder="Текстовое поле" error />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea placeholder="Текстовое поле" disabled />
-      </div>
-      <div className="w-[250px] p-4">
-        <Textarea />
-      </div> */}
-    </>
+      <Textarea variant={'s'} />
+      <Textarea maxLength={50} />
+      <Textarea variant={'s'} maxLength={50} />
+      <Textarea placeholder="Многострочное поле" />
+      <Textarea variant={'s'} placeholder="Многострочное поле" />
+      <Textarea error />
+      <Textarea error variant={'s'} />
+      <Textarea warning />
+      <Textarea warning variant={'s'} />
+      <Textarea disabled placeholder="Многострочное поле" />
+      <Textarea disabled variant={'s'} placeholder="Многострочное поле" />
+    </div>
   );
 }
--- packages/pkg.textarea/Textarea.tsx
+++ packages/pkg.textarea/Textarea.tsx
@@ -3,195 +3,54 @@ import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@xipkg/utils';
 
-// export const inputVariants = cva(
-//   'flex w-full rounded-md border-2 border-gray-30 bg-gray-0 text-gray-80 hover:border-gray-50 active:border-gray-30 focus:border-gray-80 focus-visible:outline-none px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-30 disabled:cursor-not-allowed disabled:bg-gray-10',
-//   {
-//     variants: {
-//       variant: {
-//         m: 'h-12 px-3 rounded-lg text-[16px]',
-//         s: 'h-8 px-2 rounded-md text-[14px]',
-//       },
-//       error: {
-//         true: 'border-red-80 hover:border-red-80 active:border-red-80 focus:border-red-80',
-//         false: '',
-//       },
-//       warning: {
-//         true: 'border-orange-80 hover:border-orange-80 active:border-orange-80 focus:border-orange-80',
-//         false: '',
-//       },
-//       beforeBool: {
-//         true: '',
-//         false: '',
-//       },
-//       afterBool: {
-//         true: '',
-//         false: '',
-//       },
-//     },
-//     compoundVariants: [
-//       {
-//         variant: 'm',
-//         beforeBool: true,
-//         className: 'pl-11',
-//       },
-//       {
-//         variant: 's',
-//         beforeBool: true,
-//         className: 'pl-7',
-//       },
-//       {
-//         variant: 'm',
-//         afterBool: true,
-//         className: 'pr-11',
-//       },
-//       {
-//         variant: 's',
-//         afterBool: true,
-//         className: 'pr-7',
-//       },
-//     ],
-//     defaultVariants: {
-//       variant: 'm',
-//       error: false,
-//       warning: false,
-//       beforeBool: false,
-//       afterBool: false,
-//     },
-//   },
-// );
-
-// export const addsVariants = cva('absolute flex items-center z-50', {
-//   variants: {
-//     variant: {
-//       m: 'h-6 top-3',
-//       s: 'h-4 top-2',
-//     },
-//     beforeBool: {
-//       true: '',
-//       false: '',
-//     },
-//     afterBool: {
-//       true: '',
-//       false: '',
-//     },
-//   },
-//   compoundVariants: [
-//     {
-//       variant: 'm',
-//       beforeBool: true,
-//       className: 'left-3',
-//     },
-//     {
-//       variant: 's',
-//       beforeBool: true,
-//       className: 'left-2',
-//     },
-//     {
-//       variant: 'm',
-//       afterBool: true,
-//       className: 'right-3',
-//     },
-//     {
-//       variant: 's',
-//       afterBool: true,
-//       className: 'right-2',
-//     },
-//   ],
-//   defaultVariants: {
-//     variant: 'm',
-//     beforeBool: false,
-//     afterBool: false,
-//   },
-// });
-
-// export interface InputProps
-//   extends React.InputHTMLAttributes<HTMLInputElement>,
-//     VariantProps<typeof inputVariants> {
-//   before?: React.ReactNode;
-//   after?: React.ReactNode;
-//   beforeClassName?: string;
-//   afterClassName?: string;
-//   beforeProps?: React.HTMLAttributes<HTMLDivElement>;
-//   afterProps?: React.HTMLAttributes<HTMLDivElement>;
-// }
-
-// const Textarea = React.forwardRef<HTMLInputElement, InputProps>(
-//   (
-//     {
-//       className,
-//       variant,
-//       error,
-//       warning,
-//       type,
-//       before,
-//       after,
-//       beforeClassName,
-//       afterClassName,
-//       beforeProps,
-//       afterProps,
-//       ...props
-//     },
-//     ref,
-//   ) => {
-//     return (
-//       <div className="relative">
-//         {!!before && (
-//           <div
-//             className={cn(
-//               addsVariants({
-//                 variant,
-//                 beforeBool: !!before,
-//                 afterBool: !!after,
-//                 className: beforeClassName,
-//               }),
-//             )}
-//             {...beforeProps}
-//           >
-//             {before}
-//           </div>
-//         )}
-//         <input
-//           type={type}
-//           className={cn(
-//             inputVariants({
-//               variant,
-//               error,
-//               warning,
-//               beforeBool: !!before,
-//               afterBool: !!after,
-//               className,
-//             }),
-//           )}
-//           ref={ref}
-//           {...props}
-//         />
-//         {!!after && (
-//           <div
-//             className={cn(
-//               addsVariants({
-//                 variant,
-//                 beforeBool: !!before,
-//                 afterBool: !!after,
-//                 className: afterClassName,
-//               }),
-//             )}
-//             {...afterProps}
-//           >
-//             {after}
-//           </div>
-//         )}
-//       </div>
-//     );
-//   },
-// );
-// Textarea.displayName = 'Textarea';
+export const textareaVariants = cva(
+  'flex w-full min-w-[250px] max-w-[600px] rounded-md border-2 border-gray-30 bg-gray-0 text-gray-80 hover:border-gray-50 active:border-gray-30 focus:border-gray-80 focus-visible:outline-none px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-30 disabled:cursor-not-allowed disabled:bg-gray-10 resize-none',
+  {
+    variants: {
+      variant: {
+        m: 'rounded-lg text-[16px]',
+        s: 'rounded-md text-[14px]',
+      },
+      error: {
+        true: 'border-red-80 hover:border-red-80 active:border-red-80 focus:border-red-80',
+        false: '',
+      },
+      warning: {
+        true: 'border-orange-80 hover:border-orange-80 active:border-orange-80 focus:border-orange-80',
+        false: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'm',
+      error: false,
+      warning: false,
+    },
+  },
+);
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+  VariantProps<typeof textareaVariants> { }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, rows = 3, maxRows = 15, ...props }, ref)=> {
+  (
+    {
+      className,
+      variant = 'm',
+      error,
+      warning,
+      rows = 2,
+      maxLength = 0,
+      ...props
+    },
+    ref,
+  ) => {
+    const maxRows = 16
+    const lineHeight = variant === 'm' ? 24 : 21;
+
     const textareaRef = React.useRef<HTMLTextAreaElement | null>(null);
+    const [isFocused, setIsFocused] = React.useState(false);
+    const [charCount, setCharCount] = React.useState(0);
 
     React.useEffect(() => {
       if (textareaRef.current) {
@@ -202,41 +61,65 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     const autoResize = () => {
       const textarea = textareaRef.current;
       if (textarea) {
-        // Сброс высоты, чтобы пересчитать её корректно
         textarea.style.height = 'auto';
-        
-        // Устанавливаем новую высоту с учетом текущего контента
+
         const scrollHeight = textarea.scrollHeight;
-        const maxHeight = 21 * maxRows; // Предполагаемая высота строки около 24px
-        textarea.style.height = `${Math.min(scrollHeight, maxHeight)}px`;
-        
-        // Добавление полосы прокрутки, если превышен максимум
-        textarea.style.overflowY = scrollHeight > maxHeight ? 'scroll' : 'hidden';
+
+        const currentRows = Math.ceil(scrollHeight / lineHeight);
+        const newRows = Math.min(currentRows + 1, maxRows);
+
+        const newHeight = newRows * lineHeight;
+        textarea.style.height = `${newHeight}px`;
+
+        textarea.style.overflowY = newRows >= maxRows ? 'scroll' : 'hidden';
       }
     };
 
-    return (
-      <textarea
-        className={cn(
-          "flex w-full min-w-[250px] max-w-[600px] rounded-md border-2 border-gray-30 bg-gray-0 text-gray-80 hover:border-gray-50 active:border-gray-30 focus:border-gray-80 focus-visible:outline-none px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-30 disabled:cursor-not-allowed disabled:bg-gray-10 resize-none",
-          className
-        )}
-        ref={(el) => {
-          textareaRef.current = el;
-          if (typeof ref === 'function') {
-            ref(el);
-          } else if (ref) {
-            (ref as React.MutableRefObject<HTMLTextAreaElement | null>).current = el;
-          }
-        }}
-        rows={rows}
-        onInput={autoResize}
-        {...props}
-      />
-    )
-  }
-)
-Textarea.displayName = "Textarea"
+    const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setCharCount(e.target.value.length);
+      autoResize();
+    };
 
+    return (
+      <div className="relative w-full max-w-[600px]">
+        <textarea
+          className={cn(
+            textareaVariants({
+              variant,
+              error,
+              warning,
+              className,
+            }),)
+          }
+          ref={(el) => {
+            textareaRef.current = el;
+            if (typeof ref === 'function') {
+              ref(el);
+            } else if (ref) {
+              (ref as React.MutableRefObject<HTMLTextAreaElement | null>).current = el;
+            }
+          }}
+          rows={rows}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onInput={handleInput}
+          {...props}
+        />
+        {maxLength > 0 && isFocused && (
+          <div
+            className={cn(
+              "absolute right-2 bottom-2 text-sm",
+              charCount > maxLength ? 'text-red-80' : 'text-gray-50'
+            )}
+          >
+            {maxLength - charCount}
+          </div>
+        )}
+
+      </div>
+    );
+  }
+);
+Textarea.displayName = 'Textarea';
 
 export { Textarea };
