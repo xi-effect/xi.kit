06deb220590bafae20f036ac7806425f4344a701
d9m0n4 <ched08@yandex.ru>
2025-01-27 21:10:13 +0300

    fix(35672106): fix packages import

--- apps/xi.storybook/vite.config.ts
+++ apps/xi.storybook/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    preserveSymlinks: true,
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@storybook/addon-docs'],
   },
 });
