c012932b1311848f6aacf8348248c37ba82ea280
unknownproperty <igor.buksheff@yandex.ru>
2025-02-14 20:14:59 +0300

    feat: remove dist and add it to gitignore

--- packages/pkg.input.mask/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as react from 'react';
-
-type MaskTypeT = 'card' | 'passport' | 'phone' | 'inviteCode' | 'inviteCodeUrl' | 'date' | 'time' | 'dateRange' | 'any';
-
-declare const useMaskInput: (mask?: MaskTypeT) => react.RefCallback<HTMLElement>;
-
-export { useMaskInput };
