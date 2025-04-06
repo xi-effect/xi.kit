bb335b466197e75c253f861db84d20877e145ac8
unknownproperty <igor.buksheff@yandex.ru>
2025-04-06 15:47:03 +0300

    fix: sidebar css var with tailwind v4 bug

--- packages/pkg.sidebar/const.ts
+++ packages/pkg.sidebar/const.ts
@@ -1,5 +1,5 @@
 export const SIDEBAR_COOKIE_NAME = 'sidebar_state';
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-export const SIDEBAR_WIDTH = '20rem';
-export const SIDEBAR_WIDTH_ICON = '3rem';
+export const SIDEBAR_WIDTH = '350px';
+export const SIDEBAR_WIDTH_ICON = '72px';
 export const SIDEBAR_KEYBOARD_SHORTCUT = 'b';
