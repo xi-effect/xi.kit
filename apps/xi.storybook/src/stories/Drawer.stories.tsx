0bde04bf3317d20d444a3e2141448ce3a7b8ccc6
reinaka <juliabednaia@gmail.com>
2025-06-06 11:23:16 +0300

    fix(49497652): drawer story style fix

--- apps/xi.storybook/src/stories/Drawer.stories.tsx
+++ apps/xi.storybook/src/stories/Drawer.stories.tsx
@@ -20,25 +20,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const Template = () => (
-  <div style={{ display: 'flex', gap: '12px' }}>
-    <Drawer>
-      <DrawerTrigger asChild>
-        <Button size="s">Открыть дровер</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Заголовок</DrawerTitle>
-        </DrawerHeader>
-        <div className="bg-violet-20 text-opacity-40 flex h-[449px] w-full items-center justify-center rounded-[8px] text-violet-100">
-          Content area
-        </div>
-        <DrawerFooter className="flex flex-row gap-4">
-          <Button type="submit">Кнопка</Button>
-          <Button variant="ghost">Кнопка</Button>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  </div>
+  <Drawer>
+    <DrawerTrigger asChild>
+      <Button size="s">Открыть дровер</Button>
+    </DrawerTrigger>
+    <DrawerContent>
+      <DrawerHeader>
+        <DrawerTitle>Заголовок</DrawerTitle>
+      </DrawerHeader>
+      <div className="bg-violet-20 text-opacity-40 flex h-[449px] w-full items-center justify-center rounded-[8px] text-violet-100">
+        Content area
+      </div>
+      <DrawerFooter className="flex flex-row gap-4">
+        <Button type="submit">Кнопка</Button>
+        <Button variant="ghost">Кнопка</Button>
+      </DrawerFooter>
+    </DrawerContent>
+  </Drawer>
 );
 
 export const Default: Story = {
