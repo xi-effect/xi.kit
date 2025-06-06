06a5b21eaed6979efd89b4b01539dc076b40ddd6
reinaka <juliabednaia@gmail.com>
2025-06-06 10:55:36 +0300

    fix(49497652): fix drawer styles

--- apps/xi.storybook/src/stories/Drawer.stories.tsx
+++ apps/xi.storybook/src/stories/Drawer.stories.tsx
@@ -1,25 +1,19 @@
 import { Meta, StoryObj } from '@storybook/react';
 import {
-    Drawer,
-    DrawerContent,
-    DrawerTrigger,
-    DrawerHeader,
-    DrawerFooter,
-    DrawerTitle,
-} from "@xipkg/drawer";
-import { Button } from "@xipkg/button";
-
-// type DrawerStoryArgs = DrawerContentProps &
-//   ModalHeaderProps &
-//   ModalFooterProps &
-//   VariantProps<typeof contentVariants>;
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+} from '@xipkg/drawer';
+import { Button } from '@xipkg/button';
 
 const meta = {
   title: 'Components/Drawer',
   component: DrawerContent,
   tags: ['autodocs'],
-  argTypes: {
-  },
+  argTypes: {},
 } as Meta;
 
 export default meta;
@@ -35,7 +29,9 @@ const Template = () => (
         <DrawerHeader>
           <DrawerTitle>Заголовок</DrawerTitle>
         </DrawerHeader>
-        <div className="bg-violet-20 h-[449px] text-violet-100 text-opacity-40 rounded-[8px] flex items-center justify-center w-full">Content area</div>
+        <div className="bg-violet-20 text-opacity-40 flex h-[449px] w-full items-center justify-center rounded-[8px] text-violet-100">
+          Content area
+        </div>
         <DrawerFooter className="flex flex-row gap-4">
           <Button type="submit">Кнопка</Button>
           <Button variant="ghost">Кнопка</Button>
