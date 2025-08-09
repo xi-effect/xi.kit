371585c00a2ac1571ffd14f58c3d44b0ae8d370f
unknownproperty <igor.buksheff@yandex.ru>
2025-08-09 23:44:21 +0300

    feat(eslint): up config

--- packages/pkg.eslint/index.cjs
+++ packages/pkg.eslint/index.cjs
@@ -1,17 +1,25 @@
-// eslint.config.js / eslint.config.mjs / index.js (ESM)
+// eslint.config.js (ESM)
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
 
 export default tseslint.config(
   { ignores: ['dist'] },
+
+  // База JS + базовые правила TS
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  // TS/TSX-исходники с явным парсером
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: tsParser,
       globals: globals.browser,
     },
     plugins: {
@@ -19,8 +27,7 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
-      // было: ...reactHooks.configs.recommended.rules
-      // стало: «правильно» для ESLint 9 flat config
+      // актуальный пресет для ESLint 9
       ...reactHooks.configs['recommended-latest'].rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       semi: ['error', 'always'],
