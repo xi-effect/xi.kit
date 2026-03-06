49a6296bbb34ca47c891a0606c0fd943c3f31630
unknownproperty <igor.buksheff@yandex.ru>
2026-03-07 01:13:35 +0300

    feat(pkg.slider): add custom classNames for slider

--- packages/pkg.slider/Slider.tsx
+++ packages/pkg.slider/Slider.tsx
@@ -7,17 +7,39 @@ import { cn } from '@xipkg/utils';
 
 interface SliderProps extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> {
   ref?: React.Ref<React.ElementRef<typeof SliderPrimitive.Root>>;
+  /** Кастомные стили для трека слайдера */
+  trackClassName?: string;
+  /** Кастомные стили для заполненной части трека (range) */
+  rangeClassName?: string;
+  /** Кастомные стили для ползунка (thumb) */
+  thumbClassName?: string;
 }
 
-export const Slider = ({ className, ...props }: SliderProps) => (
+export const Slider = ({
+  className,
+  trackClassName,
+  rangeClassName,
+  thumbClassName,
+  ...props
+}: SliderProps) => (
   <SliderPrimitive.Root
     className={cn('relative flex w-full touch-none items-center select-none', className)}
     {...props}
   >
-    <SliderPrimitive.Track className="bg-gray-30 relative h-1 w-full grow overflow-hidden rounded-lg">
-      <SliderPrimitive.Range className="bg-brand-80 absolute h-full" />
+    <SliderPrimitive.Track
+      className={cn(
+        'bg-gray-30 relative h-1 w-full grow overflow-hidden rounded-lg',
+        trackClassName
+      )}
+    >
+      <SliderPrimitive.Range className={cn('bg-brand-80 absolute h-full', rangeClassName)} />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="bg-brand-80 ring-offset-background block h-4 w-4 cursor-pointer rounded-full transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb
+      className={cn(
+        'bg-brand-80 ring-offset-background block h-4 w-4 cursor-pointer rounded-full transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50',
+        thumbClassName
+      )}
+    />
   </SliderPrimitive.Root>
 );
 
