c47065faded5888849828bad3f16b43e0858da60
d9m0n4 <ched08@yandex.ru>
2026-06-28 22:48:57 +0300

    feat(164): add stickers component

--- packages/pkg.emoji.picker/EmojiPickerPopup.tsx
+++ packages/pkg.emoji.picker/EmojiPickerPopup.tsx
@@ -16,12 +16,13 @@ import {
 } from '@xipkg/icons';
 import { Input } from '@xipkg/input';
 import { cn } from '@xipkg/utils';
-import { CategoryT, TEmojiPickerProps, EmojiT } from './types';
+import { CategoryT, TEmojiPickerPropsT, EmojiT, StickerT } from './types';
 import { EmojiCategory } from './EmojiCategory';
 import emojisData from './emojis.json';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@xipkg/tooltip';
+import { Tabs } from '@xipkg/tabs';
 
-type TEmojiPopup = TEmojiPickerProps & {
+type TEmojiPopupT = TEmojiPickerPropsT & {
   className?: string;
 };
 
@@ -38,7 +39,12 @@ const categoryIcons = [
   { icon: Flag, name: 'Флаги' },
 ];
 
-export const EmojiPickerPopup = ({ recentEmojis, onEmojiSelect }: TEmojiPopup) => {
+export const EmojiPickerPopup = ({
+  recentEmojis,
+  onEmojiSelect,
+  stickers,
+  onStickerSelect,
+}: TEmojiPopupT) => {
   const [activeCategoryIndex, setActiveCategoryIndex] = useState<number>(0);
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const [searchQuery, setSearchQuery] = useState<string>('');
@@ -125,82 +131,115 @@ export const EmojiPickerPopup = ({ recentEmojis, onEmojiSelect }: TEmojiPopup) =
     [onEmojiSelect],
   );
 
+  const handleStickerClick = useCallback(
+    (sticker: StickerT) => {
+      onStickerSelect?.(sticker);
+    },
+    [onStickerSelect],
+  );
+
   return (
-    <div className="flex h-[296px] w-[276px]">
-      <div
-        tabIndex={0}
-        aria-label="Категории эмодзи"
-        className="bg-gray-5 flex flex-col gap-2 rounded-l-lg p-2"
-      >
-        <TooltipProvider>
-          {emojiCategoriesIcons.map(({ icon: Icon, name }, index) => {
-            return (
-              <Tooltip key={index}>
-                <TooltipTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    className={cn(
-                      'group h-auto rounded-[4px] border-transparent p-1 hover:border-transparent focus:border-transparent',
-                      index === activeCategoryIndex
-                        ? 'bg-brand-0 text-brand-100 hover:bg-brand-0'
-                        : 'bg-transparent text-brand-60 hover:bg-brand-0 hover:text-brand-80',
-                    )}
-                    onClick={() => selectCategory(index)}
-                  >
-                    <Icon
-                      size="sm"
-                      className={
-                        index === activeCategoryIndex ? 'text-brand-100' : 'text-brand-60'
-                      }
-                    />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent side="left" className="z-99">
-                  <p>{name}</p>
-                </TooltipContent>
-              </Tooltip>
-            );
-          })}
-        </TooltipProvider>
-      </div>
-      <div className="bg-gray-0 flex w-full flex-col gap-2 rounded-r-lg p-2">
-        <Input
-          variant="s"
-          before={<Search size="sm" className="text-gray-60" />}
-          placeholder="Поиск"
-          className="border"
-          value={searchQuery}
-          onChange={handleSearchEmoji}
-        />
-        <div
-          className="h-full overflow-y-auto"
-          ref={scrollContainerRef}
-          style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
-        >
-          {filteredCategories && searchQuery ? (
-            <EmojiCategory
-              setActiveCategory={setActiveCategoryIndex}
-              category={filteredCategories}
-              currentIndex={0}
-              handleEmojiClick={handleEmojiClick}
-              containerRef={scrollContainerRef}
-              isIntersectionEnabled={false}
-            />
-          ) : (
-            emojiCategories.length > 0 &&
-            emojiCategories.map((emojis, index) => (
-              <EmojiCategory
-                key={index}
-                setActiveCategory={setActiveCategoryIndex}
-                category={emojis}
-                currentIndex={index}
-                handleEmojiClick={handleEmojiClick}
-                containerRef={scrollContainerRef}
+    <div className="bg-gray-0 h-auto w-69 rounded-lg">
+      <Tabs.Root defaultValue="emoji" className="flex h-full max-h-91 min-h-0 w-full flex-col">
+        <Tabs.List className="py-1.5">
+          <Tabs.Trigger value="emoji">Эмодзи</Tabs.Trigger>
+          <Tabs.Trigger value="stickers">Стикеры</Tabs.Trigger>
+        </Tabs.List>
+
+        <Tabs.Content value="stickers" className="overflow-y-auto">
+          <div className="h-full overflow-y-auto p-2">
+            <div className="grid grid-cols-3 gap-2">
+              {stickers?.map((sticker) => (
+                <Button
+                  key={sticker.id}
+                  variant="ghost"
+                  onClick={() => handleStickerClick(sticker)}
+                  className="hover:bg-gray-5 flex aspect-square size-full items-center justify-center rounded p-1 transition-colors"
+                >
+                  <img
+                    src={sticker.src}
+                    alt={sticker.name}
+                    className="max-h-full max-w-full object-contain"
+                  />
+                </Button>
+              ))}
+            </div>
+          </div>
+        </Tabs.Content>
+
+        <Tabs.Content value="emoji" className="flex min-h-0 flex-1">
+          <div className="relative flex min-h-0 w-full">
+            <div
+              tabIndex={0}
+              aria-label="Категории эмодзи"
+              className="bg-gray-5 flex flex-col gap-2 rounded-bl-lg p-2"
+            >
+              <TooltipProvider>
+                {emojiCategoriesIcons.map(({ icon: Icon, name }, index) => (
+                  <Tooltip key={index}>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className={cn(
+                          'h-auto rounded-sm border-transparent p-1',
+                          index === activeCategoryIndex
+                            ? 'bg-brand-0 text-brand-100'
+                            : 'text-brand-60',
+                        )}
+                        onClick={() => selectCategory(index)}
+                      >
+                        <Icon size="sm" />
+                      </Button>
+                    </TooltipTrigger>
+
+                    <TooltipContent side="left">
+                      <p>{name}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                ))}
+              </TooltipProvider>
+            </div>
+
+            <div className="bg-gray-0 absolute right-0 flex h-full min-h-0 flex-1 flex-col gap-2 rounded-r-lg p-2">
+              <Input
+                variant="s"
+                before={<Search size="sm" className="text-gray-60" />}
+                placeholder="Поиск"
+                className="border"
+                value={searchQuery}
+                onChange={handleSearchEmoji}
               />
-            ))
-          )}
-        </div>
-      </div>
+
+              <div
+                ref={scrollContainerRef}
+                className="min-h-0 flex-1 overflow-x-hidden overflow-y-auto"
+              >
+                {searchQuery ? (
+                  <EmojiCategory
+                    setActiveCategory={setActiveCategoryIndex}
+                    category={filteredCategories}
+                    currentIndex={0}
+                    handleEmojiClick={handleEmojiClick}
+                    containerRef={scrollContainerRef}
+                    isIntersectionEnabled={false}
+                  />
+                ) : (
+                  emojiCategories.map((category, index) => (
+                    <EmojiCategory
+                      key={index}
+                      setActiveCategory={setActiveCategoryIndex}
+                      category={category}
+                      currentIndex={index}
+                      handleEmojiClick={handleEmojiClick}
+                      containerRef={scrollContainerRef}
+                    />
+                  ))
+                )}
+              </div>
+            </div>
+          </div>
+        </Tabs.Content>
+      </Tabs.Root>
     </div>
   );
 };
